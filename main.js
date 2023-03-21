b05ac06badbcf91490244d324d285e6159cd27f4
Silencepls <viniciustorrescancela@gmail.com>
2023-03-20 21:09:10 -0300

    Adding Functionality to Buttons

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-// ready to put logic
+let farmlaxArray = [
+    "images/Farmlax.png",
+    "images/Farmlax2.png",
+    "images/Farmlax3.png"
+];
+let flIndex = 0;
+
+let leftArrowFL = document.getElementById("farmlax-left");
+leftArrowFL.addEventListener("click", () => {
+    flIndex--;
+    leftArrowFL.previousElementSibling.src = farmlaxArray[Math.abs(flIndex) % 3];
+});
+
+let rightArrowFL = document.getElementById("farmlax-right");
+rightArrowFL.addEventListener("click", () => {
+    flIndex++
+    rightArrowFL.previousElementSibling.previousElementSibling.src = farmlaxArray[Math.abs(flIndex) % 3];
+});
