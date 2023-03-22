6006d33991e506384ef8f286bfb87ece9eb5d096
Silencepls <viniciustorrescancela@gmail.com>
2023-03-21 21:35:09 -0300

    Solved a bug with image and buttons

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const farmlaxArray = [
     "images/Farmlax2.png",
     "images/Farmlax3.png"
 ];
+let loadedFl = [];
+let flIndex = 0;
 
 const artsArray = [
     "images/arts/Soul.jpg",
@@ -12,54 +14,51 @@ const artsArray = [
     "images/arts/4 ink - Ostra.jpg",
     "images/arts/Monogatari ed 1 Full.png",
     "images/arts/11 ink - Eargle.jpg"
-];
+]
+let loadedArts = [];
+let artIndex = 0;
 
-const imagesToPreload = [...farmlaxArray, ...artsArray];
+let loadImages = new Promise((resolve) => {
+    for(let i = 0; i < artsArray.length; i++){
+        let img = new Image();
+        img.src = artsArray[i];
+        loadedArts.push(img);
+    }
+    for(let i = 0; i < farmlaxArray.length; i++){
+        let img = new Image();
+        img.src = farmlaxArray[i];
+        loadedFl.push(img);
+    }
+    resolve();
+})
 
-const preloadedImages = [];
+loadImages.then(() => {
+    const leftArrowFL = document.querySelectorAll(".arrow.left-arrow");
+    const rightArrowFL = document.querySelectorAll(".arrow.right-arrow");
+    const leftArrowArt = document.querySelectorAll(".short-arrow.arrow.left-arrow");
+    const rightArrowArt = document.querySelectorAll(".short-arrow.arrow.right-arrow");
 
-Promise.all(imagesToPreload.map(url => {
-    return new Promise((resolve, reject) => {
-        const img = new Image();
-        img.onload = () => {
-            preloadedImages[url] = img;
-            resolve();
-        };
-        img.onerror = () => {
-            reject(`Failed to load image ${url}`);
-        };
-        img.src = url;
-    });
-}))
-    .then(() => {
-        const leftArrowFL = document.getElementById("farmlax-left");
-        const rightArrowFL = document.getElementById("farmlax-right");
-        const leftArrowArt = document.querySelectorAll(".short-arrow.arrow.left-arrow");
-        const rightArrowArt = document.querySelectorAll(".short-arrow.arrow.right-arrow");
-
-        let flIndex = 0;
-        let artIndex = 0;
-
-        leftArrowFL.addEventListener("click", () => {
-            flIndex--;
-            leftArrowFL.previousElementSibling.src = preloadedImages[farmlaxArray[(flIndex + farmlaxArray.length) % farmlaxArray.length]].src;
-        });
-
-        rightArrowFL.addEventListener("click", () => {
-            rightArrowFL.previousElementSibling.previousElementSibling.src = preloadedImages[farmlaxArray[(flIndex + 1) % farmlaxArray.length]].src;
-            flIndex = (flIndex + 1) % farmlaxArray.length;
-        });
-
-        leftArrowArt[0].addEventListener("click", () => {
-            artIndex--;
-            leftArrowArt[0].previousElementSibling.src = preloadedImages[artsArray[(artIndex + artsArray.length) % artsArray.length]].src;
-        });
-
-        rightArrowArt[0].addEventListener("click", () => {
-            rightArrowArt[0].previousElementSibling.previousElementSibling.src = preloadedImages[artsArray[(artIndex + 1) % artsArray.length]].src;
-            artIndex = (artIndex + 1) % artsArray.length;
-        });
+    leftArrowFL[0].addEventListener("click", () => {
+        flIndex == 0 ? flIndex = farmlaxArray.length - 1 : flIndex--;
+        let val = flIndex % farmlaxArray.length;
+        leftArrowFL[0].previousElementSibling.src = loadedFl[val].src;
     })
-    .catch((error) => {
-        console.error(error);
+
+    rightArrowFL[0].addEventListener("click", () => {
+        flIndex++;
+        let val = flIndex % farmlaxArray.length;
+        rightArrowFL[0].previousElementSibling.previousElementSibling.src = loadedFl[val].src;
     });
+
+    leftArrowArt[0].addEventListener("click", () => {
+        artIndex == 0 ? artIndex = artsArray.length - 1 : artIndex--;
+        let val = artIndex % artsArray.length;
+        leftArrowArt[0].previousElementSibling.src = loadedArts[val].src;
+    });
+
+    rightArrowArt[0].addEventListener("click", () => {
+        artIndex++;
+        let val = artIndex % artsArray.length;
+        rightArrowArt[0].previousElementSibling.previousElementSibling.src = loadedArts[val].src;
+    });
+})
